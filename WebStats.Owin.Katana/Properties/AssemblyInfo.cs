272bbf344e1ad262b3e8e6538ef3c4c7904003c5
Diego B Fernandez <diegobfernandez@outlook.com>
2014-04-16 21:58:24 -0300

    Managing Nuget stuff

--- WebStats.Owin.Katana/Properties/AssemblyInfo.cs
+++ WebStats.Owin.Katana/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("630d9843-5a33-4724-bebe-cb4da104f124")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
